592ac7927e936bd42f0c1072ac7438dcb7967f93
Menuka Ishan <menukaishan5@gmail.com>
2014-10-28 19:21:26 +0530

    Re-initiating 2

--- DB_Connect.php
+++
@@ -1,8 +0,0 @@
-<?php
-$Registration_DB_Connect = mysqli_connect("localhost","root","","ucscconf") ;
-// Check connection
-if (mysqli_connect_errno()) {
-  echo "Failed to connect to MySQL: " . mysqli_connect_error();
-}
-
-?>
