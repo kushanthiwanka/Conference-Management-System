0ee59968fe3b08040a272e47c6c0b12a283cf03b
Menuka Ishan <menukaishan5@gmail.com>
2014-11-11 10:31:08 +0530

    Reg and login secure

---
+++ phpSecureLogin-master/includes/psl-config.php
@@ -0,0 +1,9 @@
+<?php
+
+define("HOST", "localhost"); 			// The host you want to connect to. 
+define("USER", "root"); 			// The database username. 
+define("PASSWORD", ""); 	// The database password. 
+define("DATABASE", "ucsc_conf");             // The database name.
+define("CAN_REGISTER", "any");
+define("DEFAULT_ROLE", "member");
+define("SECURE", FALSE);    // For development purposes only!!!!
---
+++ phpSecureLogin-master/register.php
@@ -0,0 +1,79 @@
+<?php
+
+include_once 'includes/register.inc.php';
+include_once 'includes/functions.php';
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Secure Login: Registration Form</title>
+        <script type="text/JavaScript" src="js/sha512.js"></script> 
+        <script type="text/JavaScript" src="js/forms.js"></script>
+        <link rel="stylesheet" href="styles/main.css" />
+    </head>
+    <body>
+        
+        <h1>Register with us</h1>
+        <?php
+        if (!empty($error_msg)) {
+            echo $error_msg;
+        }
+        ?>
+        <ul>
+            <li>Usernames may contain only digits, upper and lower case letters and underscores</li>
+            <li>Emails must have a valid email format</li>
+            <li>Passwords must be at least 6 characters long</li>
+            <li>Passwords must contain
+                <ul>
+                    <li>At least one upper case letter (A..Z)</li>
+                    <li>At least one lower case letter (a..z)</li>
+                    <li>At least one number (0..9)</li>
+                </ul>
+            </li>
+            <li>Your password and confirmation must match exactly</li>
+        </ul>
+        <fieldset style="width:40%">
+            <legend align="center">Registration Form</legend>
+            <table>
+        <form method="post" name="registration_form" action="<?php echo esc_url($_SERVER['PHP_SELF']); ?>">
+            <tr>
+                <td> Username: </td><td><input type='text' name='username' id='username' /><br></td>
+            </tr>
+            <tr>
+                <td>Email: </td><td><input type="text" name="email" id="email" /><br></td>
+            </tr>
+            <tr>
+                <td>Password: </td><td>
+                    <input type="password"
+                             name="password" 
+                             id="password"/><br>
+                </td>
+            </tr>
+            <tr>
+                <td>Confirm password: </td><td>
+                    <input type="password" 
+                                     name="confirmpwd" 
+                                     id="confirmpwd" /><br>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <input type="button" 
+                   value="Register" 
+                   onclick="return regformhash(this.form,
+                                   this.form.username,
+                                   this.form.email,
+                                   this.form.password,
+                                   this.form.confirmpwd);" /> 
+                </td>
+            </tr>
+            
+        </form>
+
+        </table>
+        </fieldset>
+        <p>Return to the <a href="index.php">login page</a>.</p>
+    </body>
+</html>
