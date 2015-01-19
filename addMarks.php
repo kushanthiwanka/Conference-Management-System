f7230e76925f9d73ac5e55e3d1fe62f7a9e776d9
krishanlakmal <krishanlakmal123@gmail.com>
2015-01-19 09:06:05 +0530

    kiriya part

---
+++ addMarks.php
@@ -0,0 +1,40 @@
+
+<html>
+<form name="htmlform" method="post" action="html_form_send.php">
+<table width="450px">
+</tr>
+<tr>
+ <td valign="top">
+  <label for="first_name">Paper ID*</label>
+ </td>
+ <td valign="top">
+  <input  type="text" name="paperId" maxlength="50" size="30">
+ </td>
+</tr>
+ 
+<tr>
+ <td valign="top"">
+  <label for="last_name">Mark*</label>
+ </td>
+ <td valign="top">
+  <input  type="text" name="mark" maxlength="50" size="30">
+ </td>
+</tr>
+
+<tr>
+ <td valign="top">
+  <label for="comments">Comments *</label>
+ </td>
+ <td valign="top">
+  <textarea  name="comments" maxlength="1000" cols="25" rows="6"></textarea>
+ </td>
+ 
+</tr>
+<tr>
+ <td colspan="2" style="text-align:center">
+  <input type="submit" value="Submit"> 
+ </td>
+</tr>
+</table>
+</form>
+</html>
