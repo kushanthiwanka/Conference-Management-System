217458ebad7d0d5557f2381aaef246317df40e67
Menuka Ishan <menukaishan5@gmail.com>
2015-01-20 17:17:05 +0530

    Update

--- upload.php
+++ upload.php
@@ -1,56 +1,40 @@
 <?php
 
-// Create connection
-
-$target_dir = "uploads/";
-$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-$uploadOk = 1;
-$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
-
-print_r($target_file);
-echo '<br>';
-
-// Check if file already exists
-if (file_exists($target_file)) {
-    echo "Sorry, file already exists.";
-    $uploadOk = 0;
-}
-
-// Allow certain file formats
-if($imageFileType != "pdf" ) {
-    echo "Sorry, only PDF files are allowed.";
-    $uploadOk = 0;
-}
-// Check if $uploadOk is set to 0 by an error
-if ($uploadOk == 0) {
-    echo "Sorry, your file was not uploaded.";
-// if everything is ok, try to upload file
-} 
-else {
-    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-        echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
-    } else {
-        echo "Sorry, there was an error uploading your file.";
-    }
-}
-
 // Check connection
 
 $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "ucsc_conf";
+$state = 1;
+if (isset($_POST['upload']) && $_FILES['userfile']['size'] > 0) {
+    $fileName = $_FILES['userfile']['name'];
+    $tmpName = $_FILES['userfile']['tmp_name'];
+    $fileSize = $_FILES['userfile']['size'];
+    $fileType = $_FILES['userfile']['type'];
+    $fileType = (get_magic_quotes_gpc() == 0 ? mysql_real_escape_string(
+                            $_FILES['userfile']['type']) : mysql_real_escape_string(
+                            stripslashes($_FILES['userfile'])));
+	
+	$fp = fopen($tmpName, 'r');
+    $content = fread($fp, filesize($tmpName));
+    $content = addslashes($content);
+    fclose($fp);
+    if (!get_magic_quotes_gpc()) {
+        $fileName = addslashes($fileName);
+    }
 
-
-$authorMail = $_SESSION["email"];
-$paperTitle = $_POST["paperTitle"];
+$authorMail = filter_input(INPUT_POST, 'contactemail');
+$paperTitle = filter_input(INPUT_POST, 'paperTitle');
 $date = date("Y/m/d") ;
-$pathofPaper="../uploads/". basename( $_FILES["fileToUpload"]["name"]);
-$firstname = $_POST["firstname"];
-$middlename = $_POST["middlename"];
-$lastname = $_POST["lastname"];
-$organization = $_POST["organization"];
-$country = $_POST["country"];
+
+//$pathofPaper="../uploads/". basename( $_FILES["fileToUpload"]["name"]);
+
+$firstname = filter_input(INPUT_POST, 'firstname');
+$middlename = filter_input(INPUT_POST, 'middlename');
+$lastname = filter_input(INPUT_POST, 'lastname');
+$organization = filter_input(INPUT_POST, 'organization');
+$country = filter_input(INPUT_POST, 'country');
 
 
 if(isset($_POST['topic'])){
@@ -59,13 +43,16 @@ if(isset($_POST['topic'])){
     }
 }
 $topic = $topicArea;
-$keywords = $_POST["keywords"];
-$abstract = $_POST["abstract"];
-$comments = $_POST["comments"];
-$contactname = $_POST["contactname"];
-$contactemail = $_POST["contactemail"];
-$contactnumber=$_POST["contactnumber"];
+$keywords = filter_input(INPUT_POST, 'keywords');
+$abstract = filter_input(INPUT_POST, 'abstract');
+$comments = filter_input(INPUT_POST, 'comments');
+$contactname = filter_input(INPUT_POST, 'contactname');
 
+$contactnumber=filter_input(INPUT_POST, 'contactnumber');
+
+$con = mysql_connect('localhost', 'root', '') or die(mysql_error());
+$db = mysql_select_db('test', $con);
+/*
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 
 if ($conn->connect_error) {
@@ -74,14 +61,25 @@ if ($conn->connect_error) {
         echo 'connected to database';
     }
    
+*/
 
+if ($db) {
 
-$sql = "INSERT INTO research_paper ( AuthorMail, PaperTitle, Date, PathOfPaper, firstName, middleName, lastName, organization, country, topicAriea, keyWord, abstrakt, optionalComment, contactName, contactEmail, contactNumber) VALUES('$authorMail', '$paperTitle', '$date','$pathofPaper', '$firstname','$middlename', '$lastname', '$organization', '$country', '$topic', '$keywords', '$abstract', '$comments', '$contactname', '$contactemail', '$contactnumber')";
+        $sql = "INSERT INTO research_paper ( AuthorMail,state, PaperTitle, Date, firstName, middleName, lastName, organization, country, topicAriea, keyWord, abstract, optionalComment, contactName, contactNumber, name, size, type, content)". "VALUES ('$authorMail', '$state','$paperTitle', '$date', '$firstname','$middlename', '$lastname', '$organization', '$country', '$topic', '$keywords', '$abstract', '$comments', '$contactname', '$contactnumber', '$fileName', '$fileSize', '$fileType', '$content')";
 
+            mysql_query($sql) or die('Error, query failed');
+            mysql_close();
+            echo "<br>File $fileName uploaded<br>";
+	}else {
+        echo "file upload failed";
+        }
+/*
 if ($conn->query($sql) === TRUE) {
     echo "New record created successfully";
 } else {
     echo "Error: " ."<br>" . $conn->error;
 }
 
-$conn->close();
+*/
+
+}
